4735d499ac9948a04798027d45611130e66a103a
Vitor Mateus Romancini do Amaral <77472862+Vitormateusromancini@users.noreply.github.com>
2023-06-15 11:11:37 -0300

    Filas

---
+++ Filas.c
@@ -0,0 +1,10 @@
+/*A estrutura de Fila é idêntica a que usamos no
+nosso dia a dia: o primeiro que entra em uma
+fila é o primeiro a ser atendido.
+– Um elemento deve ser sempre introduzido no
+final da fila.
+– O único elemento que pode ser retirado da
+fila é o do início.
+– FIFO (First In , First Out).*/
+
+
