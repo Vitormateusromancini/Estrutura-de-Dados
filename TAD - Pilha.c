97f8620e677a69e4c0b34f56982c91df35c35411
Vitor Mateus Romancini do Amaral <77472862+Vitormateusromancini@users.noreply.github.com>
2023-06-15 10:50:36 -0300

    TAD - pilhas

---
+++ TAD - Pilha.c
@@ -0,0 +1,33 @@
+/*A ideia fundamental da pilha é que todo acesso
+aos seus elementos é feito através do topo.
+– Cada elemento introduzido na pilha passa a
+ser o elemento do topo.
+– O único elemento que pode ser retirado da
+pilha é o topo.
+– Lembre-se de uma pilha de pratos.
+– LIFO (Last In , First Out).
+– Funções principais Push (empilhar) e Pop
+(desempilhar)
+
+
+Um exemplo típico de pilhas é a própria pilha de
+execução da linguagem C (já vimos como ela
+funciona nos exemplos de recursividade).
+
+
+Considere uma pilha que armazena valores
+reais.
+O TAD-Pilha sera composto de 5 operacoes:
+-Criar uma pilha vazia.
+-Inserir um elemento no topo (push)
+-Remover um elemento do topo (pop)
+-Verificar se a pilha esta vazia
+-Liberar a estrutura da pilha
+
+A interface pilha.h pode conter o seguinte
+código.
+
+
+******inserir o código*********
+
+
