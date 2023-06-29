abfbb4e9cc45424688b2fb23cc64d447868d05ae
Vitor Mateus Romancini do Amaral <77472862+Vitormateusromancini@users.noreply.github.com>
2023-06-29 10:41:09 -0300

    grafos

---
+++ Grafos.c
@@ -0,0 +1,11 @@
+/*Definição
+
+*Como representar um conjunto de objetos
+e as suas relacoes?
+-Diversos tipos de aplicacoes necessitam disso
+-Um grafo e um modelo matematico que
+representa as relacoes entre objetos de um
+determinado conjunto.
+
+
+*/
